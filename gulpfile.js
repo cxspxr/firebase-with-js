e59c83223308add8cdf1467a0fe833f052e31d7e
casper <ycasperinho@gmail.com>
2018-03-12 14:56:01 +0200

    make gulpfile to reload changes

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const bs = require('browser-sync');
+
+
+gulp.task('server', function() {
+    bs.init({
+        server: './',
+        ghostMode: false,
+        open: false,
+        notify: false
+    });
+});
+
+gulp.task('watch', function() {
+    gulp.watch('index.html', function() {
+        bs.reload();
+    });
+});
+
+gulp.task('default', ['server', 'watch']);
